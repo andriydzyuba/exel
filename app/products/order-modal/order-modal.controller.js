f1ba6c52bd73494a32c2371554b8079c96b9b6a2
AndriyDzyuba <andriydzyuba91@gmail.com>
2017-11-03 19:16:19 +0200

    fixed modal

--- app/products/order-modal/order-modal.controller.js
+++ app/products/order-modal/order-modal.controller.js
@@ -5,22 +5,23 @@ angular.module('exel')
 
 
 
-.controller('OrderModalController', ['$scope', '$location', '$state', '$stateParams', 'productsService', 'modalsService', 'productId', 'ordersService',
-             function($scope, $location, $state, $stateParams, productsService, modalsService, productId, ordersService) {
- 
+.controller('OrderModalController', ['$scope', '$location', '$state', '$stateParams', 'productsService', 'modalsService', 'productId', 'ordersService', '$modalStack',
+             function($scope, $location, $state, $stateParams, productsService, modalsService, productId, ordersService, $modalStack) {
+
     productsService.getProduct(productId).then(function(data){
         $scope.product = data;
         $scope.order = {
         	product_id: $scope.product.id
         };
     })
-   
+
 
     $scope.createOrder = function(order) {
     	console.log($scope.order);
+        $modalStack.dismissAll();
         ordersService.createOrder($scope.order).then(function(data){
-           
+
         })
     }
 
-}])  
+}])
