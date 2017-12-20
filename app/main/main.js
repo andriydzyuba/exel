7f4c02242f65b3e234c24c43b7650331680621fc
AndriyDzyuba <andriydzyuba91@gmail.com>
2017-12-20 18:20:49 +0200

    inko 1.1 - fixed littel bug

--- app/main/main.js
+++ app/main/main.js
@@ -348,7 +348,7 @@ angular.module('exel.main', ['ngRoute', 'dcbImgFallback'])
 
     $scope.category = {};
 
-   // for dropMenu
+   /* for dropMenu
     $(document).ready(function(){
       $(".dropSubMenu").hover(function(e){
         $(this).next("ul").toggle();
@@ -358,7 +358,8 @@ angular.module('exel.main', ['ngRoute', 'dcbImgFallback'])
         $(this).toggle();
         e.stopPropagation();
       });
-    });
+    });*/
+
     // for serch
     $(document).ready(function(){
       $("#clickSearch").focusin(function(){
